242c5577a13d54a42062c82a4b8e7e952ca27273
张宁 <cheungling@zhangning-MacBook-Pro.local>
2018-01-25 15:51:05 +0800

    [ADD] github first learn

---
+++ YZN_first_learn/YZN_First_learn/YZN_First_learn/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YZN_First_learn
+//
+//  Created by 张宁 on 2018/1/25.
+//  Copyright © 2018年 XXX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
